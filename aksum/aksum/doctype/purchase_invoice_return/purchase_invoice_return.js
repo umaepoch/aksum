714e515767bc44514fcb9e1eda21a857e3c0ed6d
AkashMunikoti <akashmunikoti@gmail.com>
2022-07-31 17:49:53 +0530

    aksum doctypes removed

--- aksum/aksum/doctype/purchase_invoice_return/purchase_invoice_return.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2022, Pavithra M R and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('Purchase Invoice Return', {
-	// refresh: function(frm) {
-
-	// }
-});
